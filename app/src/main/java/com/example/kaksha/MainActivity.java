4097f89058fe4535d834425c0fddfce70e023b4e
Gaurav <gauravrai568@gmail.com>
2019-09-19 05:19:15 +0530

    Home screen added
    TinyDB added
    Java 8 support added
    Other useful libraries added

--- app/src/main/java/com/example/kaksha/MainActivity.java
+++ app/src/main/java/com/example/kaksha/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kaksha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.go).setOnClickListener(v -> {
+
+            startActivity(new Intent(MainActivity.this, Home.class));
+
+        });
+
     }
 }
