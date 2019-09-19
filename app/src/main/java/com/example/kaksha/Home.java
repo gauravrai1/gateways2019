0b7d1fd5ce6331cf265cfc44d39d48ad7d070d73
Gaurav <gauravrai568@gmail.com>
2019-09-19 07:51:25 +0530

    Home screen recyclerView added
    -required libs added
    -name dialog added

--- app/src/main/java/com/example/kaksha/Home.java
+++ app/src/main/java/com/example/kaksha/Home.java
@@ -1,14 +1,40 @@
 package com.example.kaksha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.kaksha.Adapter.RecyclerViewCardAdapter;
+import com.example.kaksha.Model.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Home extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewCardAdapter adapter;
+    private List<Card> cards = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Home.this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new RecyclerViewCardAdapter(Home.this);
+        cards.add(new Card("Hey"));
+        cards.add(new Card("Hey"));
+        cards.add(new Card("Hey"));
+        cards.add(new Card("Hey"));
+        adapter.setListContent(cards);
+        recyclerView.setAdapter(adapter);
+
     }
+
 }
