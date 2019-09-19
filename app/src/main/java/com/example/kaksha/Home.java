44a7ea7d786ac0151eb136d9eef2fac0aeeadb5c
Gaurav <gauravrai568@gmail.com>
2019-09-19 10:35:56 +0530

    Question test page

--- app/src/main/java/com/example/kaksha/Home.java
+++ app/src/main/java/com/example/kaksha/Home.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.kaksha.Adapter.RecyclerViewCardAdapter;
+import com.example.kaksha.DB.TinyDB;
 import com.example.kaksha.Model.Card;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ public class Home extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerViewCardAdapter adapter;
     private List<Card> cards = new ArrayList<>();
+    private TinyDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +28,20 @@ public class Home extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        db = new TinyDB(Home.this);
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(Home.this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
         adapter = new RecyclerViewCardAdapter(Home.this);
         cards.add(new Card("Hey"));
-        cards.add(new Card("Hey"));
-        cards.add(new Card("Hey"));
-        cards.add(new Card("Hey"));
+        cards.add(new Card("Good morning"));
+        cards.add(new Card("Cheerful Vibes"));
+        cards.add(new Card("Let's play a game?"));
         adapter.setListContent(cards);
         recyclerView.setAdapter(adapter);
+
+        TextView name = findViewById(R.id.name);
+        name.setText(db.getString("name"));
 
     }
 
